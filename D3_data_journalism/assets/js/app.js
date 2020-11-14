8ce4b17a5944362c3000f51a06a27fd62e8546e2
Eric Viera <ericviera@Erics-MBP.home>
2020-11-14 14:56:19 -0500

    chart margins defined

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,18 @@
 // @TODO: YOUR CODE HERE!
+
+// Define SVG Area Dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var charMargin = {
+    top: 100,
+    right: 100,
+    bottom: 100,
+    left: 100
+};
+
+// Define the dimensions of the chart area
+var chartWidth = svgWidth - charMargin.left - charMargin.right;
+var chartHeight = svgHeight - charMargin.top - charMargin.bottom;
+
