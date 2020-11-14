82aff553112a22349a7a2fff37849e0ff86f95e9
Eric Viera <ericviera@Erics-MBP.home>
2020-11-14 15:12:06 -0500

    Data Loaded

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -5,7 +5,7 @@ var svgWidth = 960;
 var svgHeight = 660;
 
 // Define the chart's margins as an object
-var charMargin = {
+var chartMargin = {
     top: 100,
     right: 100,
     bottom: 100,
@@ -13,8 +13,8 @@ var charMargin = {
 };
 
 // Define the dimensions of the chart area
-var chartWidth = svgWidth - charMargin.left - charMargin.right;
-var chartHeight = svgHeight - charMargin.top - charMargin.bottom;
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select id "scatter", append svg area to it and set dimensions
 var svg = d3.select("#scatter")
@@ -22,7 +22,19 @@ var svg = d3.select("#scatter")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
 
+// Append a group to the SVG area and shift ('translate') it to the right
+// and down to adhere to the margins set in "chartMargin" object
 
+var chartGroup = svg.append("g")
+    .attr("transform",`translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load CSV Data
+d3.csv("assets/data/data.csv").then(function(data){
+    //Print Data
+    console.log(data);
+}
+
+);
 
 
 
